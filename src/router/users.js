1bc555bf1a8400b16f5291d172a20d4d24fa015c
Grandhys Setyo Utomo <setyo.grandhys1@gmail.com>
2021-03-06 19:14:38 +0700

    add response json

--- src/router/users.js
+++ src/router/users.js
@@ -7,12 +7,30 @@ let users = [
 ]
 
 router.get('/users', function(req, res){
-    res.json(users)
+    if(users.length){
+        res.json({
+            status: true,
+            data: users,
+            method: req.method,
+            url: req.url
+        })
+    } else {
+        res.json({
+            status: false,
+            message: 'Data users is empty'
+        })
+    }
 })
 
 router.post('/users', function(req, res){
     users.push(req.body)
-    res.send(users)
+    res.send({
+        status: true,
+        data: users,
+        message: 'Data saved successfully!!',
+        method: req.method,
+        url: req.url
+    })
 })
 
 router.put('/users/:id', function(req, res){
@@ -21,14 +39,28 @@ router.put('/users/:id', function(req, res){
             user.id = req.params.id
             user.name = req.body.name
             user.email = req.body.email
+
+            return user
         }
     })
-    res.send(users)
+    res.json({
+        status: true,
+        data: users,
+        message: 'Data has been successfully updated!!',
+        method: req.method,
+        url: req.url
+    })
 })
 
 router.delete('/users/:id', function(req, res){
     users = users.filter(user => user.id != req.params.id)
-    res.send(users)
+    res.send({
+        status: true,
+        data: users,
+        message: 'Data has been successfully deleted!!',
+        method: req.method,
+        url: req.url
+    })
 })
 
 module.exports = router
